d9808d4aa1947a3423772cab4f57cf7c9f08071a
GeffersonST <geffsevero@gmail.com>
2021-10-30 22:08:06 -0300

    Inserindo Imagens

--- src/pages/CreatePoint/index.jsx
+++ src/pages/CreatePoint/index.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import "./style.css";
 import logo from "../../assets/logo.svg";
+import oleo from "../../assets/oleo.svg";
 const CreatePoint = () => {
   return (
     <div id="page-create-point">
@@ -13,6 +14,64 @@ const CreatePoint = () => {
           Voltar para Home
         </Link>
       </header>
+
+      <form>
+        <h1>
+          Cadastro do <br /> Ponto de Coleta
+        </h1>
+        <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+          <div className="field">
+            <label htmlFor="name">Nome da Entidade</label>
+            <input type="text" name="name" id="name" />
+          </div>
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="email">E-mail</label>
+              <input type="email" name="email" id="email" />
+            </div>
+            <div className="field">
+              <label htmlFor="whatsapp">Whatsapp</label>
+              <input type="text" name="whatsapp" id="whatsapp" />
+            </div>
+          </div>
+        </fieldset>
+        <fieldset>
+          <legend>
+            <h2>Endereço</h2>
+            <span>Selecione o endereço no Mapa</span>
+          </legend>
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="uf">Estado (UF)</label>
+              <select name="uf" id="uf">
+                <option value="0">Selecione uma UF</option>
+              </select>
+            </div>
+            <div className="field">
+              <label htmlFor="city">Cidade</label>
+              <select name="city" id="city">
+                <option value="0">Selecione uma Cidade</option>
+              </select>
+            </div>
+          </div>
+        </fieldset>
+        <fieldset>
+          <legend>
+            <h2>Ítens de Coleta</h2>
+            <span>Selecione um ou mais ítens abaixo</span>
+          </legend>
+
+          <ul className="items-grid">
+            <li>
+              <img src={oleo} alt="oleo" />
+              <span>Óleo de Cozinha</span>
+            </li>
+          </ul>
+        </fieldset>
+      </form>
     </div>
   );
 };
