f6ba3ea587555fd93e6bae6d7a5ffd280558c7ea
GeffersonST <geffsevero@gmail.com>
2021-10-30 22:14:03 -0300

    Imagens Final

--- src/pages/CreatePoint/index.jsx
+++ src/pages/CreatePoint/index.jsx
@@ -4,6 +4,11 @@ import { FiArrowLeft } from "react-icons/fi";
 import "./style.css";
 import logo from "../../assets/logo.svg";
 import oleo from "../../assets/oleo.svg";
+import baterias from "../../assets/baterias.svg";
+import lampadas from "../../assets/lampadas.svg";
+import organicos from "../../assets/organicos.svg";
+import papeis from "../../assets/papeis-papelao.svg";
+import eletronicos from "../../assets/eletronicos.svg";
 const CreatePoint = () => {
   return (
     <div id="page-create-point">
@@ -68,6 +73,26 @@ const CreatePoint = () => {
             <li>
               <img src={oleo} alt="oleo" />
               <span>Óleo de Cozinha</span>
+            </li>
+            <li>
+              <img src={baterias} alt="baterias" />
+              <span>Baterias</span>
+            </li>
+            <li>
+              <img src={lampadas} alt="lampadas" />
+              <span>Lâmpadas</span>
+            </li>
+            <li>
+              <img src={organicos} alt="organicos" />
+              <span>Orgânicos</span>
+            </li>
+            <li>
+              <img src={papeis} alt="papeis" />
+              <span>Papéis e Papelão</span>
+            </li>
+            <li>
+              <img src={eletronicos} alt="eletronicos" />
+              <span>Eletrônicos</span>
             </li>
           </ul>
         </fieldset>
